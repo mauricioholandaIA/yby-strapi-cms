94caa67ad900dedef5810480697ac481754719bf
julioatlantico <julio_goncalves@atlantico.com.br>
2024-12-03 16:31:30 -0300

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('4GL4CMsvX9vUNfNtS9HDQg=='),
+    secret: '4GL4CMsvX9vUNfNtS9HDQg==',
   },
   apiToken: {
-    salt: env('jiwnE1YVzuyAbFwaH5KT6A=='),
+    salt: 'jiwnE1YVzuyAbFwaH5KT6A==',
   },
   transfer: {
     token: {
-      salt: env('5DMzr2sqSBE09lhm3DXyzg=='),
+      salt: '5DMzr2sqSBE09lhm3DXyzg==',
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: true,
+    promoteEE: true,
   },
 });
