f2d6ca82e719c2b6cdf8856e73f8c9d615fdc377
julioatlantico <julio_goncalves@atlantico.com.br>
2024-12-03 14:11:38 -0300

    Create server.js

---
+++ config/server.js
@@ -0,0 +1,9 @@
+const { config } = require('process');
+
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+});
