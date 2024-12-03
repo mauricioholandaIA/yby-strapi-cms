122c5732c5e8481e71abef8fe4817ebebd382253
julioatlantico <julio_goncalves@atlantico.com.br>
2024-12-03 15:46:25 -0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -4,6 +4,8 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['rwSPVKNi9c/SaJfsBs5y/Q==', '3tj699Un1YmQDF5SkwY5Vg==', 'xg6OuzXxxbeX7RrHDlTZTg==', 'mLbSK8TNVgE2p5PBLgwNPw==']), 
   },
 });
+
+
