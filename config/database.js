73d3096a08365db69125dec54ba44e61aeb9713a
julioatlantico <julio_goncalves@atlantico.com.br>
2024-12-03 14:10:50 -0300

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+module.exports = ({ env }) => {
+  const client = env("DATABASE_CLIENT", "postgres");
+
+  const connections = {
+    postgres: {
+      connection: {
+        connectionString: env("DATABASE_URL"),
+        host: env("DATABASE_HOST", "terraform-20241129052344326800000001.cxo6swgqm3n3.us-east-1.rds.amazonaws.com"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "yby_db_dev"),
+        user: env("DATABASE_USERNAME", "postgresadmin"),
+        password: env("DATABASE_PASSWORD", "JDu384nf8294h"),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          key: env("DATABASE_SSL_KEY", undefined),
+          cert: env("DATABASE_SSL_CERT", undefined),
+          ca: env("DATABASE_SSL_CA", undefined),
+          capath: env("DATABASE_SSL_CAPATH", undefined),
+          cipher: env("DATABASE_SSL_CIPHER", undefined),
+          rejectUnauthorized: env.bool(
+            "DATABASE_SSL_REJECT_UNAUTHORIZED",
+            true
+          ),
+        },
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+      },
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    },
+  };
+};
